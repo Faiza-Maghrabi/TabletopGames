74375091b7c029f9a8317952257e8ab1a01238bc
Faiza <faiza.maghrabi@gmail.com>
2025-01-26 18:51:53 +0000

    chore: clean up params

--- src/main/java/games/calico/CalicoGameParameters.java
+++ src/main/java/games/calico/CalicoGameParameters.java
@@ -2,7 +2,6 @@ package games.calico;
 
 import core.AbstractParameters;
 import core.CoreConstants;
-import core.components.Counter;
 import core.components.Deck;
 import games.calico.CalicoTypes.BoardTypes;
 import games.calico.CalicoTypes.Button;
@@ -18,8 +17,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Random;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -148,68 +145,6 @@ public class CalicoGameParameters extends AbstractParameters {
 
         return catsInPlay;
     }
-
-    //TM
-    HashSet<CalicoTypes.Expansion> expansions = new HashSet<CalicoTypes.Expansion>() {{ add(CorporateEra); }};  // Elysium, Hellas and Venus compiling, but not fully parsed yet
-    int soloTR = 14;
-    int soloMaxGen = 14;
-    int soloCities = 2;
-
-    HashMap<CalicoTypes.Resource, Integer> minimumProduction = new HashMap<CalicoTypes.Resource, Integer>() {{
-        for (CalicoTypes.Resource res: CalicoTypes.Resource.values()) {
-            if (res == CalicoTypes.Resource.MegaCredit) put(res, -5);
-            else put(res, 0);
-        }
-    }};
-    HashMap<CalicoTypes.Resource, Integer> startingResources = new HashMap<CalicoTypes.Resource, Integer>() {{
-        for (CalicoTypes.Resource res: CalicoTypes.Resource.values()) {
-            put(res, 0);
-        }
-        put(CalicoTypes.Resource.TR, 20);
-//        put(TMTypes.Resource.MegaCredit, 500);  // TODO Test
-    }};
-    HashMap<CalicoTypes.Resource, Integer> startingProduction = new HashMap<CalicoTypes.Resource, Integer>() {{
-        for (CalicoTypes.Resource res: CalicoTypes.Resource.values()) {
-            if (res.isPlayerBoardRes()) {
-                put(res, 1);
-            }
-        }
-    }};
-    int maxPoints = 500;
-    int maxCards = 250;  // TODO based on expansions
-
-    int projectPurchaseCost = 3;
-    int nCorpChoiceStart = 2;
-    int nProjectsStart = 10;
-    int nProjectsResearch = 4;
-    int nActionsPerPlayer = 2;
-    int nMCGainedOcean = 2;
-
-    // steel and titanium to MC rate
-    double nSteelMC = 2;
-    double nTitaniumMC = 3;
-
-    // standard projects
-    int nGainCardDiscard = 1;
-    int nCostSPEnergy = 11;
-    int nCostSPTemp = 14;
-    int nCostSPOcean = 18;
-    int nCostSPGreenery = 23;
-    int nCostSPCity = 25;
-    int nSPCityMCGain = 1;
-    int nCostVenus = 15;
-
-    // Resource actions
-    int nCostGreeneryPlant = 8;
-    int nCostTempHeat = 8;
-
-    // Milestones, awards
-    int[] nCostMilestone = new int[] {8, 8, 8};
-    int[] nCostAwards = new int[] {8, 14, 20};
-    int nPointsMilestone = 5;
-    int nPointsAwardFirst = 5;
-    int nPointsAwardSecond = 2;
-
     @Override
     protected AbstractParameters _copy() {
         return new CalicoGameParameters();
@@ -220,127 +155,7 @@ public class CalicoGameParameters extends AbstractParameters {
         return false;
     }
 
-    public HashMap<CalicoTypes.Resource, Integer> getMinimumProduction() {
-        return minimumProduction;
-    }
-
-    public HashMap<CalicoTypes.Resource, Integer> getStartingProduction() {
-        return startingProduction;
-    }
-
-    public HashMap<CalicoTypes.Resource, Integer> getStartingResources() {
-        return startingResources;
-    }
-
     public int getBoardSize() {
         return boardSize;
-    }
-
-    public int getMaxPoints() {
-        return maxPoints;
-    }
-
-    public int getnCorpChoiceStart() {
-        return nCorpChoiceStart;
-    }
-
-    public int getProjectPurchaseCost() {
-        return projectPurchaseCost;
-    }
-
-    public int getnProjectsResearch() {
-        return nProjectsResearch;
-    }
-
-    public int getnProjectsStart() {
-        return nProjectsStart;
-    }
-
-    public HashSet<CalicoTypes.Expansion> getExpansions() {
-        return expansions;
-    }
-
-    public int getnMCGainedOcean() {
-        return nMCGainedOcean;
-    }
-
-    public int[] getnCostMilestone() {
-        return nCostMilestone;
-    }
-
-    public int[] getnCostAwards() {
-        return nCostAwards;
-    }
-
-    public double getnSteelMC() {
-        return nSteelMC;
-    }
-
-    public double getnTitaniumMC() {
-        return nTitaniumMC;
-    }
-
-    public int getnActionsPerPlayer() {
-        return nActionsPerPlayer;
-    }
-
-    public int getnCostGreeneryPlant() {
-        return nCostGreeneryPlant;
-    }
-
-    public int getnCostSPCity() {
-        return nCostSPCity;
-    }
-
-    public int getnCostSPEnergy() {
-        return nCostSPEnergy;
-    }
-
-    public int getnCostSPGreenery() {
-        return nCostSPGreenery;
-    }
-
-    public int getnCostSPOcean() {
-        return nCostSPOcean;
-    }
-
-    public int getnCostSPTemp() {
-        return nCostSPTemp;
-    }
-
-    public int getnCostTempHeat() {
-        return nCostTempHeat;
-    }
-
-    public int getnGainCardDiscard() {
-        return nGainCardDiscard;
-    }
-
-    public int getnPointsAwardFirst() {
-        return nPointsAwardFirst;
-    }
-
-    public int getnPointsAwardSecond() {
-        return nPointsAwardSecond;
-    }
-
-    public int getnPointsMilestone() {
-        return nPointsMilestone;
-    }
-
-    public int getnSPCityMCGain() {
-        return nSPCityMCGain;
-    }
-
-    public int getSoloCities() {
-        return soloCities;
-    }
-
-    public int getSoloMaxGen() {
-        return soloMaxGen;
-    }
-
-    public int getSoloTR() {
-        return soloTR;
     }
 }
